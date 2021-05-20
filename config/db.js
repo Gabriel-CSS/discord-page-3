a1076b1b269bf8064cb198892853ad2662a099b0
Gabriel-CSS <gabriel.silva.gs142@gmail.com>
2021-05-20 19:24:30 -0300

    change config db

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
-if (process.env.NODE_ENV == "development") {
-    module.exports = { mongoURI: "mongodb://localhost:27017" };
-} else {
-    module.exports = { mongoURI: "mongodb+srv://discordDB:yChJBgkxAM8HuDhw@clusterdiscord.g50kt.mongodb.net/discordDB?retryWrites=true&w=majority" };
-}
+// if (process.env.NODE_ENV == "development") {
+//     module.exports = { mongoURI: "mongodb://localhost:27017" };
+// } else {
+//     module.exports = { mongoURI: "mongodb+srv://discordDB:yChJBgkxAM8HuDhw@clusterdiscord.g50kt.mongodb.net/discordDB?retryWrites=true&w=majority" };
+// }
+
+module.exports = { mongoURI: "mongodb+srv://discordDB:yChJBgkxAM8HuDhw@clusterdiscord.g50kt.mongodb.net/discordDB?retryWrites=true&w=majority" };
