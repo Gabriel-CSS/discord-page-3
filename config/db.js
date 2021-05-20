969fc04b1e0a9bd80c76fb2a1fc74de3c3605c20
Gabriel-CSS <gabriel.silva.gs142@gmail.com>
2021-05-20 19:52:23 -0300

    fix: configdb

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
-if (process.env.NODE_ENV == "producion") {
-    module.exports = { mongoURI: "mongodb+srv://discordDB:yChJBgkxAM8HuDhw@clusterdiscord.g50kt.mongodb.net/discordDB?retryWrites=true&w=majority" };
-} else {
-    module.exports = { mongoURI: "mongodb://localhost:27017" };
-}
+// if (process.env.NODE_ENV == "producion") {
+//     module.exports = { mongoURI: "mongodb+srv://discordDB:yChJBgkxAM8HuDhw@clusterdiscord.g50kt.mongodb.net/discordDB?retryWrites=true&w=majority" };
+// } else {
+//     module.exports = { mongoURI: "mongodb://localhost:27017" };
+// }
+
+module.exports = { mongoURI: "mongodb+srv://discordDB:yChJBgkxAM8HuDhw@clusterdiscord.g50kt.mongodb.net/discordDB?retryWrites=true&w=majority" };
